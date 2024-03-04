c015db79e587ea5d161b6a4de061da7515e57711
Pranab Kumar <pranabkumars54@gmail.com>
2024-03-04 22:12:39 +0530

    todo app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddTodo from "./componenets/AddTodo";
+import Todo from "./componenets/Todo";
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
